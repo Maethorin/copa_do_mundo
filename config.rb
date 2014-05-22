f55a3bb1600a187b54b370abcd44e2863951a92e
Márcio Duarte <maethorin@gmail.com>
2014-05-22 14:27:29 -0300

    Incluindo bootstrap-sass!

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require 'bootstrap-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -8,7 +9,7 @@ images_dir = "tabela/static/img"
 javascripts_dir = "tabela/static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
