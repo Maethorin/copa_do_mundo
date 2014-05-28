c423c83467422a4478f18b22df7b1a39ac935dee
Márcio Duarte <maethorin@gmail.com>
2014-05-28 19:40:10 -0300

    Animando paineis... to animadinho!!! HAHAHAHAHA

--- tabela/static/js/inicial.js
+++ tabela/static/js/inicial.js
@@ -2,8 +2,19 @@ $(window).load(function() {
     $('.barras').animate({top: -20}, 600);
     $('.introducao').animate({top: 0}, 600, function() {
         $('.introducao .texto').slideToggle(600, function() {
-            $('.partidas-index .painel').show().animate({bottom: 20})
-            $('.barras-baixo').show().animate({top: 40})
+            $('.partidas-index .painel').show().animate({bottom: 20});
+            $('.barras-baixo').show().animate({top: 40});
         });
     });
 });
+
+$(window).unload(function() {
+    $('.barras-baixo').show().animate({top: 66});
+    $('.partidas-index .painel').show().animate({bottom: -52}, function() {
+        $('.introducao .texto').slideToggle(600, function() {
+            $('.barras').animate({top: 105}, 600);
+            $('.introducao').animate({top: -75}, 600);
+        });
+    });
+});
+
